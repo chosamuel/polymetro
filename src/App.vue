4c2d61dccf3e6b1e58998c7531b13a0028ef9bf2
Samuel Cho <samuelcho@gmx.de>
2020-02-25 13:48:45 +0100

    added inc and dec of 1

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,23 @@
 	<div id="app">
 		<vue-p5 @setup="setup" @draw="draw"></vue-p5>
 		<div>
-				<div class="clickable" @click="inc10">+10</div>
-				<div class="clickable" @click="dec10">-10</div>
-			<div class="tempo-display" @click="toggleTempoInput" v-if="!tempoInput">{{ tempo }}</div>
-			<input v-if="tempoInput" class="tempo-display" v-model="tempo" @keyup.enter="toggleTempoInput">
+			<div class="clickable" @click="dec1">-1</div>
+			<div class="clickable" @click="dec10">-10</div>
+			<div class="clickable" @click="inc10">+10</div>
+			<div class="clickable" @click="inc1">+1</div>
+			<div
+				class="tempo-display"
+				@click="toggleTempoInput"
+				v-if="!tempoInput"
+			>
+				{{ tempo }}
+			</div>
+			<input
+				v-if="tempoInput"
+				class="tempo-display"
+				v-model="tempo"
+				@keyup.enter="toggleTempoInput"
+			/>
 			<div @click="toggle" v-if="!toggled" class="clickable">
 				PLAY
 			</div>
@@ -29,10 +42,10 @@ export default {
 	data: () => ({
 		wave: null,
 		toggled: false,
-			tempoInput: false,
+		tempoInput: false,
 		tempo: 60,
 		id: null,
-			interval: null,
+		interval: null
 	}),
 
 	methods: {
@@ -47,10 +60,8 @@ export default {
 		toggle() {
 			this.toggled = !this.toggled;
 			if (this.toggled) {
-
 				this.wave.start();
 				this.bang();
-
 			}
 
 			if (!this.toggled) {
@@ -58,38 +69,46 @@ export default {
 				this.wave.stop();
 			}
 		},
-			bang(){
+		bang() {
 			this.interval = (60 / this.tempo) * 1000;
-				this.wave.amp(1);
-					setTimeout(() => { this.wave.amp(0) }, 40);
-					this.id = setTimeout(this.bang,this.interval)
+			this.wave.amp(1);
+			setTimeout(() => {
+				this.wave.amp(0);
+			}, 40);
+			this.id = setTimeout(this.bang, this.interval);
+		},
+		inc10() {
+			this.tempo += 10;
+		},
+		dec10() {
+			this.tempo -= 10;
+		},
+			inc1(){
+					this.tempo += 1;
 			},
-			inc10 (){
-					this.tempo +=10
+			dec1(){
+					this.tempo -= 1;
 			},
-			dec10(){
-					this.tempo -=10
-			},
-			toggleTempoInput(){
-					this.tempoInput = !this.tempoInput
-			}
+		toggleTempoInput() {
+			this.tempoInput = !this.tempoInput;
+		}
 	}
 };
 </script>
 
 <style>
 .clickable {
-		margin: 20px;
-		padding: 20px;
-		border: solid black;
-		border-radius: 10%;
-		display: block;
-		cursor: pointer;
-		text-align: center;
+	margin: 20px;
+	padding: 20px;
+	border: solid black;
+	border-radius: 10%;
+	display: block;
+	cursor: pointer;
+	text-align: center;
 }
 .tempo-display {
-		text-align: center;
-		font-size: 3rem;
-		background: lightgreen;
+	text-align: center;
+	font-size: 3rem;
+	background: lightgreen;
 }
 </style>
